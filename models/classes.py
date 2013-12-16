d478c21665b22bdb17639729bc57155ed593561a
Alexandre BM <s@rednaks.tn>
2013-12-16 13:26:34 +0100

    Updating tables and columns name

--- models/classes.py
+++ models/classes.py
@@ -1,20 +1,19 @@
 from common import *
-
-class Personne(Base):
-  __tablename__ = 'personne'
-  nCin = Column(Integer, primary_key = True)
+  
+class Etudiant(Base):
+  __tablename__ = 'etudiant'
+  numInscription = Column('num_insc', String(15), primary_key = True)
+  nCin = Column('cin', String(8))
   nom = Column(String(90))
   prenom = Column(String(90))
   adresse = Column(String(90))
   email = Column(String(90), unique = True)
   sexe = Column(String(90))
-  date_naissance = Column(Date)
-  type = Column(String(20))
+  date_naissance = Column('dat_nai', Date)
+  classe = Column('classe_id', String(15), ForeignKey('classe.id'))
 
-  __mapper_args__ = { 'polymorphic_identity':'personne',
-                      'polymorphic_on':type}
-
-  def __init__(self, nCin, nom, prenom, adresse, email, sexe, date):
+  def __init__(self, numInscription, nCin, nom, prenom, adresse, sexe, date_naissance):
+    self.numInscription = numInscription
     self.nCin = nCin
     self.nom = nom
     self.prenom = prenom
@@ -22,38 +21,35 @@ class Personne(Base):
     self.sexe = sexe
     self.date_naissance = date_naissance
 
-  def __repr__(self):
-    return '<Personne %r %r>' %(self.nom, self.prenom)
-
-
-class Etudiant(Personne):
-  __tablename__ = 'etudiant'
-  id = Column(Integer, ForeignKey('personne.nCin'), unique = True)
-  numInscription = Column(String(15), primary_key = True)
-  classe = Column(String(15), ForeignKey('classe.id'))
-
-  __mapper_args__ = {'polymorphic_identity':'etudiant'}
-
-  def __init__(self, numInscription):
-    self.numInscription = numInscription
 
 
 
-class Enseignant(Personne):
+class Enseignant(Base):
   __tablename__ = 'enseignant'
-  id = Column(Integer, ForeignKey('personne.nCin'))
+  nCin = Column('cin', String(8))
+  nom = Column(String(90))
+  prenom = Column(String(90))
+  adresse = Column(String(90))
+  email = Column(String(90), unique = True)
+  sexe = Column(String(90))
+  date_naissance = Column('dat_nai', Date)
   matricule = Column(String(10), primary_key = True)
 
-  __mapper_args__ = {'polymorphic_identity':'enseignant'}
-
-  def __init__(self, matricule):
+  def __init__(self, matricule, nCin, nom, prenom, adresse, sexe, date_naissance):
     self.matricule = matricule
-    
+    self.nCin = nCin
+    self.nom = nom
+    self.prenom = prenom
+    self.adresse = adresse
+    self.sexe = sexe
+    self.date_naissance = date_naissance
+
+   
 
 class Classe(Base):
   __tablename__ = 'classe'
   id = Column(String(10), primary_key = True)
-
+ 
   def __init__(self, id, nombreEtudiant = None):
     self.id = id
     self.nombreEtudiant = nombreEtudiant
@@ -71,11 +67,11 @@ class Matiere(Base):
 class Seance(Base):
   __tablename__ = 'seance'
   id = Column(Integer, primary_key = True)
-  matiere = Column(String(20), ForeignKey('matiere.code'))
-  classe = Column(String(10), ForeignKey('classe.id'))
+  matiere = Column('matiere_code', String(20), ForeignKey('matiere.code'))
+  classe = Column('classe_id', String(10), ForeignKey('classe.id'))
   presenceList = relationship('Presence', backref='seance', lazy='dynamic')
-  date = Column(Date)
-  enseignant = Column(Integer, ForeignKey('enseignant.id'), nullable = False)
+  date = Column('Date_Se', Date)
+  enseignant = Column('enseignant_matricule', String(255), ForeignKey('enseignant.matricule'), nullable = False)
 
   def __init__(self, matiere, classe):
     self.matiere = matiere
@@ -84,8 +80,8 @@ class Seance(Base):
 class Presence(Base):
   __tablename__ = 'presence'
   id = Column(Integer, primary_key = True)
-  etudiant = Column(Integer, ForeignKey('etudiant.id'), nullable = False)
-  heure_entree = Column(Date)
+  etudiant = Column('etudiant_num_insc', Integer, ForeignKey('etudiant.num_insc'), nullable = False)
+  heure_entree = Column('h_entree', Time)
   seance_id = Column(Integer, ForeignKey('seance.id'))
   etat = Column(Boolean)
 
